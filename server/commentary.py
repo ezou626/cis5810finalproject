17952a9ac058dbe93d4042e897c3901636225b7e
Eric Zou <ezou626@gmail.com>
2024-12-03 06:40:44 -0500

    cleanup

--- server/commentary.py
+++ server/commentary.py
@@ -1,24 +1,7 @@
-import numpy as np
-import os, time, sys
-import asyncio
-import httpx
 import base64
 import cv2
-from dotenv import load_dotenv
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from PIL import Image
 import google.generativeai as genai
 gemini_model = genai.GenerativeModel(model_name = "gemini-1.5-flash")
-
-def preprocess_image(image, width: int, height: int):
-    image = cv2.resize(image, (width, height))
-    # image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    # image = image.resize((width, height), Image.Resampling.LANCZOS)
-    return image
-
-# Load BLIP model and processor
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 def extract_frames(video_url):
     video_capture = cv2.VideoCapture(video_url)
@@ -39,41 +22,6 @@ def batch_frames(frames, batch_size, framerate):
     if batch:
         yield batch
 
-def generate_caption_with_blip(image: np.ndarray, width: int, height: int) -> str:
-    """
-    Generate a caption for a single image using BLIP.
-
-    Args:
-        image (np.ndarray): The image as a NumPy array.
-
-    Returns:
-        str: The generated caption.
-    """
-
-    image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    image = image.resize((width, height), Image.Resampling.LANCZOS)
-
-    # Preprocess the image and generate the caption
-    inputs = processor(images=image, return_tensors="pt")
-    outputs = model.generate(**inputs, max_new_tokens=20)
-
-    # Decode and return the caption
-    return processor.decode(outputs[0], skip_special_tokens=True)
-
-def caption_images_with_blip(images, width: int, height: int):
-    """
-    Generate captions for a batch of images using BLIP.
-
-    Args:
-        images (list[np.ndarray]): A list of images as NumPy arrays.
-
-    Returns:
-        list[str]: A list of generated captions.
-    """
-    for image in images:
-        caption = generate_caption_with_blip(image, width, height)
-        yield caption
-
 async def caption_images_with_gemini(images, width: int, height: int):
     instruction = "Describe the sequence occuring the in these images in a single detailed sentence. Do not include any other words in your response besides the sentence."
     prompt = [
@@ -81,7 +29,7 @@ async def caption_images_with_gemini(images, width: int, height: int):
             'mime_type':'image/jpeg', 
             'data': base64.b64encode(
                         cv2.imencode('.jpeg', 
-                                     preprocess_image(image, width, height)
+                                     cv2.resize(image, (width, height))
                     )[1]).decode()
         } for image in images
     ]
