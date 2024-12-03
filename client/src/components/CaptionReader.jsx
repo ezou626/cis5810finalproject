babc9af421d02ca6e6454cfed417fd8804eb668c
Eric Zou <ezou626@gmail.com>
2024-12-03 05:25:00 -0500

    visual

--- client/src/components/CaptionReader.jsx
+++ client/src/components/CaptionReader.jsx
@@ -7,6 +7,7 @@ const CaptionReader = ({ streamUrl }) => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    if (!streamUrl) return;
     // Handle SSE connection
     const eventSource = new EventSource(
       `${backendUrl}/captions?video_url=${encodeURIComponent(streamUrl)}`);
@@ -20,6 +21,7 @@ const CaptionReader = ({ streamUrl }) => {
     eventSource.onmessage = (event) => {
       const newMessage = event.data;
       console.log(newMessage);
+      setError("");
       setMessage((prevMessage) =>newMessage);
     };
 
@@ -33,7 +35,7 @@ const CaptionReader = ({ streamUrl }) => {
     return () => {
       eventSource.close();
     };
-  }, []);
+  }, [streamUrl]);
 
   return (
     <div className="flex flex-col items-center w-1/2 md:w-1/4 bg-white rounded-lg shadow-lg p-6 space-y-6">
